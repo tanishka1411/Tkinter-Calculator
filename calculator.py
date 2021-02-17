9672a27ba58a4a7f49ba2ea236ebcedf7df9f856
Tanishka Vashisht <67412892+tanishka1411@users.noreply.github.com>
2021-02-17 15:57:50 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -20,4 +20,21 @@ Button(root, text=" 6", command=lambda: get_variables(6)).grid(row=3, column=2, 
 Button(root, text="7", command=lambda: get_variables(7)).grid(row=4, column=0, sticky=N + S + E + W)
 Button(root, text=" 8", command=lambda: get_variables(8)).grid(row=4, column=1, sticky=N + S + E + W)
 Button(root, text=" 9", command=lambda: get_variables(9)).grid(row=4, column=2, sticky=N + S + E + W)
+
+#adding other buttons to the calculator
+Button(root,text="AC",command=lambda :clear_all()).grid(row=5,column=0, sticky=N+S+E+W)
+Button(root,text=" 0",command = lambda :get_variables(0)).grid(row=5,column=1, sticky=N+S+E+W)
+Button(root,text=" .",command=lambda :get_variables(".")).grid(row=5, column=2, sticky=N+S+E+W)
+ 
+ 
+Button(root,text="+",command= lambda :get_operation("+")).grid(row=2,column=3, sticky=N+S+E+W)
+Button(root,text="-",command= lambda :get_operation("-")).grid(row=3,column=3, sticky=N+S+E+W)
+Button(root,text="*",command= lambda :get_operation("*")).grid(row=4,column=3, sticky=N+S+E+W)
+Button(root,text="/",command= lambda :get_operation("/")).grid(row=5,column=3, sticky=N+S+E+W)
+ 
+# adding extra operations
+Button(root,text="pi",command= lambda :get_operation("*3.14")).grid(row=2,column=4, sticky=N+S+E+W)
+Button(root,text="%",command= lambda :get_operation("%")).grid(row=3,column=4, sticky=N+S+E+W)
+Button(root,text="(",command= lambda :get_operation("(")).grid(row=4,column=4, sticky=N+S+E+W)
+Button(root,text="exp",command= lambda :get_operation("**")).grid(row=5,column=4, sticky=N+S+E+W)
 root.mainloop()
